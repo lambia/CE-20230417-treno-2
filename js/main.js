a85320d24de7bcc62fcd046e269493fec766fd4e
Luca Lambda <luca@lambia.it>
2023-04-17 12:06:14 +0200

    aggiunti esempi di scope

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 const bottone = document.getElementById("calcola");
 const annulla = document.getElementById("reset");
 
-let x = 0;
-
 bottone.addEventListener("click", function(){
 
     const prezzoPerKm = 0.21;
@@ -47,5 +45,46 @@ annulla.addEventListener("click", function() {
     document.getElementById("km").value = "";
     document.getElementById("fasciaEta").value = "";
 
+});
+
+/************************ FINE ESERCIZIO ********************/
+
+/* ESEMPI DI SCOPE: GLOBALE vs BLOCK */
+//Questa è globale, vale per let/const/var
+let x = 0;
+
+console.log("X: ", x); //X visibile qui
+
+if(true) {
+    x = 42;  //X visibile qui
+    
+    //dichiarata qui: var = globale. Fuori dal blocco, esiste.
+    var y = 123;
+
+    //dichiarata qui: let/const = block. Fuori dal blocco, muore.
+    let z = 42;
+
+    console.log("Y:", y); //visibile, siamo nello stesso scope
+    console.log("Z:", z); //visibile, siamo nello stesso scope
+}
+
+console.log("X: ", x);  //X visibile qui (è globale)
+console.log("Y:", y);   //Y visibile qui (è globale)
+// console.log("Z:", z);   //Z NON visibile qui (è block)
+
+let visibileInFunzioneA = 1; //Questa è globale, vale per let/const/var, sarà visibile in funzione
+annulla.addEventListener("fintoClick", function() {
+    
+    document.getElementById("name").value = "";
+    document.getElementById("km").value = "";
+    document.getElementById("fasciaEta").value = "";
+
+    console.log("VisibileInFunzioneA", visibileInFunzioneA); //é visibile, perchè globale
+    let abc = 42; //siamo in funzione, quindi è tutto function-scope (let/const/var sono uguali) e muore fuori da function
+    var xyz = 41; //siamo in funzione, quindi è tutto function-scope (let/const/var sono uguali) e muore fuori da function
+    console.log("ABC:", abc); //visibile
+    console.log("XYZ:", xyz); //visibile
 
 });
+// console.log("ABC:", abc); //NON visibile qui
+// console.log("XYZ:", xyz); //NON visibile qui
